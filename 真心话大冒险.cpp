d816276534db298f8068f383d0148d89394e16d3
hacker-Fanjun <78690857+hacker-Fanjun@users.noreply.github.com>
2021-04-03 21:17:41 +0800

    Add files via upload

--- 真心话大冒险.cpp
+++ 真心话大冒险.cpp
@@ -16,12 +16,13 @@ int main()
 	while(i=true)
 	{
 		system("cls");
-		cout<<"欢迎来到fanjun弱智到爆的真心话大冒险，选择都十分的disgusting，请广大朋友们注意"<<endl<<"是否开始？"<<endl;
+		cout<<"欢迎来到fanjun弱智到爆的真心话大冒险，选择都十分的disgusting，请广大朋友们注意"<<endl<<"是否开始？"<<endl;//提示信息 
 		cin>>input; //输入字符串 
 		if(input=="是")//判断主体 
 		{
 		
 			a=1+rand()%9;
+			{
 			if (a==1)
         printf("把你敬爱的母亲的微信给删了");
 			if (a==2)
@@ -40,12 +41,12 @@ int main()
         printf("无效选项");
 			if (a==9)
         printf("不写作业");
-       
+       } //判定组 
 		}
-		else i=false;
-		cout<<endl;	
-		Sleep(1500);
+		else i=false;//改值 
+		cout<<endl;	//换行 
+		Sleep(1500);//休眠 
 	}
 	
-	return 0;
+	return 0;//返回0 
 }
